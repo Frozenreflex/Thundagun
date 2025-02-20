b9e6a9d0c9d2f6b86c1471c2ab8e935c913c58bc
Nytra <14206961+Nytra@users.noreply.github.com>
2025-02-20 16:08:30 +0000

    2025 copyright

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,6 +10,6 @@ using System.Reflection;
     AssemblyDescription(
         "A lightning fast performance mod for Resonite. It improves performance by decoupling the FrooxEngine and Unity loops by running FrooxEngine and Unity on separate threads.")]
 [assembly: AssemblyCompany(Thundagun.Thundagun.AuthorString)]
-[assembly: AssemblyCopyright($"Copyright © 2024 {Thundagun.Thundagun.AuthorString}")]
+[assembly: AssemblyCopyright($"Copyright © 2025 {Thundagun.Thundagun.AuthorString}")]
 [assembly: AssemblyVersion(Thundagun.Thundagun.VersionString)]
 [assembly: AssemblyFileVersion(Thundagun.Thundagun.VersionString)]
